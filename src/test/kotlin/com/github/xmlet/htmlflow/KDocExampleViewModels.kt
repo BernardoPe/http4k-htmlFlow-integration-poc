b855cffeb06860193a21341c1b01baa7bf9e9b1e
BernardoPe <tgavent3@gmail.com>
2025-08-10 01:10:36 +0100

    test: add KDoc example view models and views

---
+++ src/test/kotlin/com/github/xmlet/htmlflow/KDocExampleViewModels.kt
@@ -0,0 +1,32 @@
+package com.github.xmlet.htmlflow.testviews.kdoc
+
+import org.http4k.template.ViewModel
+
+/**
+ * Test ViewModels that match the KDoc examples in findCompatibleView
+ */
+
+// Example 1: Direct match
+data class UserVm(val name: String) : ViewModel
+
+// Example 2 & 3: Inheritance chain
+open class BaseVm(open val baseContent: String) : ViewModel
+class DerivedVm(override val baseContent: String, val derivedContent: String) : BaseVm(baseContent)
+
+// Example 4: Interface match
+interface ProfileLike : ViewModel { 
+    val name: String 
+}
+
+data class PublicProfile(override val name: String, val bio: String) : ProfileLike
+
+// Example 5: Assignable fallback (no inheritance/interface chain to registered type)
+data class UnrelatedVm(val data: String) : ViewModel { // no inheritance or interface
+    override fun toString(): String = data
+}
+
+interface SecondaryInterface : ViewModel {
+    val secondary: String
+}
+
+class MultiInterfaceVm(override val name: String, override val secondary: String) : ProfileLike, SecondaryInterface
